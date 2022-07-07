9d5b6855fba99f082550c45a7b52d92707e6ccee
Jerome Marshall <jeromemarshall0@gmail.com>
2022-07-07 16:04:58 +0530

    ✨ NEW: redux init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  return <main className="App"></main>
 }
 
-export default App;
+export default App
